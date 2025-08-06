254d7e125ff301e94dce9480ac28df82f197e09f
Shaik Sadiq Basha <shaiksadiqbasha190133@gmail.com>
2025-08-06 21:19:05 +0530

    create project route updated

--- backend/routes/projects.py
+++ backend/routes/projects.py
@@ -9,6 +9,6 @@ router = APIRouter()
 def read_projects(db: Session = Depends(get_db)):
     return crud.get_projects(db)
 
-@router.post("/projects", response_model=schemas.Project)
+@router.post("/project/create", response_model=schemas.Project)
 def add_project(project: schemas.ProjectCreate, db: Session = Depends(get_db)):
     return crud.create_project(db, project)
