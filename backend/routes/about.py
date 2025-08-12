3c233faf2813d429f6440887a5c876e0147f4df1
Shaik Sadiq Basha <shaiksadiqbasha190133@gmail.com>
2025-08-12 22:44:43 +0530

    set admin successful

--- backend/routes/about.py
+++ backend/routes/about.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 import schemas, crud
 from database import get_db
-from hashing import get_password_hash
 
 router = APIRouter()
 
