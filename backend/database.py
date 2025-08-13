1af055f33b272c8da6ad6fe0a05e27fd40964192
Shaik Sadiq Basha <shaiksadiqbasha190133@gmail.com>
2025-08-13 14:23:13 +0530

    origins, uvicorn setup message setup

--- backend/database.py
+++ backend/database.py
@@ -3,13 +3,14 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from decouple import config
 
+
 DATABASE_URL = config("DATABASE_URL")
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True,        # Checks connection before using
     pool_recycle=300,          # Reconnect every 5 mins
     pool_size=5,               # Limit active connections
-    max_overflow=10
+    max_overflow=10,
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
--- backend/main.py
+++ backend/main.py
@@ -8,14 +8,24 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+origins = [
+    "https://sadiq-myportfolio.netlify.app/",
+    "http://localhost:5173"
+]
+
 # Allow frontend access
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # In production, restrict to frontend URL
+    allow_origins=origins,  # In production, restrict to frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.get("/")
+def home():
+    return {"message": "FastAPI running on Render!"}
+
 
 # Include routes
 app.include_router(projects.router)
