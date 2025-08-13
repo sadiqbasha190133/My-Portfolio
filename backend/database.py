2c7b333b7661119ff79599532cfa878f274ed157
Shaik Sadiq Basha <shaiksadiqbasha190133@gmail.com>
2025-08-13 12:38:27 +0530

    added netlify.toml for deploying

--- backend/database.py
+++ backend/database.py
@@ -4,7 +4,13 @@ from sqlalchemy.orm import sessionmaker
 from decouple import config
 
 DATABASE_URL = config("DATABASE_URL")
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,        # Checks connection before using
+    pool_recycle=300,          # Reconnect every 5 mins
+    pool_size=5,               # Limit active connections
+    max_overflow=10
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
