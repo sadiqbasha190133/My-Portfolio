8158addf9b8e746a78f1d40ff118dea067551962
Shaik Sadiq Basha <shaiksadiqbasha190133@gmail.com>
2025-08-13 12:48:46 +0530

    secured credentials

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 // Change this to your backend URL when deployed
 export const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: import.meta.env.VITE_API_URL,
 });
